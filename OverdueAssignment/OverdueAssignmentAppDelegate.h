ed829ffe5caec5d969fb5040df82566bb98ce750
Alex Paul <axp_alpaul@yahoo.com>
2013-11-08 16:59:25 -0500

    Initial Commit

---
+++ OverdueAssignment/OverdueAssignmentAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OverdueAssignmentAppDelegate.h
+//  OverdueAssignment
+//
+//  Created by Alex Paul on 11/8/13.
+//  Copyright (c) 2013 Alex Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OverdueAssignmentAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OverdueAssignment/OverdueAssignmentViewController.h
@@ -0,0 +1,13 @@
+//
+//  OverdueAssignmentViewController.h
+//  OverdueAssignment
+//
+//  Created by Alex Paul on 11/8/13.
+//  Copyright (c) 2013 Alex Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OverdueAssignmentViewController : UIViewController
+
+@end
